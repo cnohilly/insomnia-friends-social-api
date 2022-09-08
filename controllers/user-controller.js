7d05bed604fb3529ab749f4648a2f49f197148bb
ctnohilly <ctnohilly@gmail.com>
2022-09-08 00:26:04 -0400

    made layout for controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,16 @@
+const { User } = require('../models');
+
+const userController = {
+    // get all users
+
+    // get one user by id
+
+    // create a new user
+
+    // update a user by id
+
+    // delete a user by id
+
+}
+
+module.exports = userController;
