508965cb6f1f64fb64b7ca324241ccd09024fb58
ctnohilly <ctnohilly@gmail.com>
2022-09-08 18:42:37 -0400

    created thought-routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+//  /api/thoughts/
+router.route('/')
+    .get(getAllThoughts);
+
+//  /api/thoughts/<thoughtId>
+router.route('/:thoughtId')
+    .get(getThoughtById);
+
+//  /api/thoughts/<userId>
+router.route('/:userId')
+    .post(createThought);
+
+//  /api/thoughts/<userId>/<thoughtId>
+router.route('/:userId/:thoughtId')
+    .put(updateThought)
+    .put(createReaction)
+    .delete(deleteThought);
+
+// /api/thoughts/<userId>/<thoughtId>/<reactionId>
+router.route('/:userId/:thoughtId/:reactionId')
+    .delete(deleteReaction);
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -7,11 +7,12 @@ const {
     deleteUser
 } = require('../../controllers/user-controller');
 
-//   /api/users
+//  /api/users
 router.route('/')
     .get(getAllUsers)
     .post(createUser);
 
+//  /api/users/:id
 router.route('/:id')
     .get(getUserById)
     .put(updateUser)
