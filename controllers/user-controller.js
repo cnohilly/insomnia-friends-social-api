865a9c3e32baa201bf7b61d7703d83c60ba6cd57
ctnohilly <ctnohilly@gmail.com>
2022-09-08 00:33:30 -0400

    created the get and post controller functions

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,11 +2,58 @@ const { User } = require('../models');
 
 const userController = {
     // get all users
+    async getAllUsers(req, res) {
+        try {
+            const dbUserData = await User.find({})
+                .populate({
+                    path: 'thoughts',
+                    select: '-__v'
+                })
+                .populate({
+                    path: 'friends',
+                    select: '-__v'
+                })
+                .select('-__v')
+                .sort({ _id: -1 });
+            res.json(dbUserData);
+        } catch (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+
+    },
 
     // get one user by id
+    async getUserById({ params }, res) {
+        try {
+            const dbUserData = await User.findOne({ _id: params.id })
+                .populate({
+                    path: 'thoughts',
+                    select: '-__v'
+                })
+                .populate({
+                    path: 'friends',
+                    select: '-__v'
+                })
+                .select('-__v')
+                .sort({ _id: -1 });
+            res.json(dbUserData);
+        } catch (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+    },
 
     // create a new user
-
+    async createUser({ body }, res) {
+        try {
+            const dbUserData = await User.create(body);
+            res.json(dbUserData);
+        } catch (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+    }
     // update a user by id
 
     // delete a user by id
